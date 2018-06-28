f2d06ec75e4b4a52c6b24f2840a7b040df0114da
Young <youngook.kim@samsung.com>
2018-06-28 10:46:54 +0000

    added the task that makes pip to be latest

--- packer/bootstrap.sh
+++ packer/bootstrap.sh
@@ -3,6 +3,9 @@ while [ ! -f /var/lib/cloud/instance/boot-finished ]; do echo 'Waiting for cloud
 
 sudo apt-get update
 sudo apt-get install -y libpython2.7-dev python-pip libffi-dev libssl-dev
+
+sudo pip install --upgrade pip
+
 cat <<EOT > /tmp/requirements.txt
 ansible==2.4.0
 awscli
